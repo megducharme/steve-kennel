33f16c3d8ca8a5634f0bab4a7813abb9f88e6c08
Megan Ducharme <ducharme.meg@gmail.com>
2018-10-22 15:30:16 -0500

    updated remote URL

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,4 @@
 export default {
     // remoteURL: "https://nss-kitty-purry.herokuapp.com"
-    remoteURL: "https://md-fe-capstone-server.herokuapp.com/"
+    remoteURL: "https://md-fe-capstone-server.herokuapp.com"
 }
